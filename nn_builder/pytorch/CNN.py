0b57a1b724e671f180dc544bc78ccb4b12b5aef1
Piyush Dubey <piyushdubey1996@gmail.com>
2019-10-20 02:15:01 +0530

    Added Maxunpool layers to PyTorch CNNs

--- nn_builder/pytorch/CNN.py
+++ nn_builder/pytorch/CNN.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 from nn_builder.pytorch.Base_Network import Base_Network
 
+
 class CNN(nn.Module, Base_Network):
     """Creates a PyTorch convolutional neural network
     Args:
@@ -29,12 +30,15 @@ class CNN(nn.Module, Base_Network):
 
     NOTE that this class' forward method expects input data in the form: (batch, channels, height, width)
     """
+
     def __init__(self, input_dim, layers_info, output_activation=None,
                  hidden_activations="relu", dropout=0.0, initialiser="default", batch_norm=False,
-                 y_range= (), random_seed=0, converted_from_tf_model=False):
+                 y_range=(), random_seed=0, converted_from_tf_model=False):
         nn.Module.__init__(self)
-        self.valid_cnn_hidden_layer_types = {'conv', 'maxpool', 'avgpool', 'adaptivemaxpool', 'adaptiveavgpool', 'linear'}
-        self.valid_layer_types_with_no_parameters = [nn.MaxPool2d, nn.AvgPool2d, nn.AdaptiveAvgPool2d, nn.AdaptiveMaxPool2d]
+        self.valid_cnn_hidden_layer_types = {
+            'conv', 'maxpool', 'maxunpool', 'avgpool', 'adaptivemaxpool', 'adaptiveavgpool', 'linear'}
+        self.valid_layer_types_with_no_parameters = [
+            nn.MaxPool2d, nn.MaxUnpool2d, nn.AvgPool2d, nn.AdaptiveAvgPool2d, nn.AdaptiveMaxPool2d]
         Base_Network.__init__(self, input_dim, layers_info, output_activation, hidden_activations, dropout, initialiser,
                               batch_norm, y_range, random_seed)
         self.converted_from_tf_model = converted_from_tf_model
@@ -60,23 +64,28 @@ class CNN(nn.Module, Base_Network):
         """Checks that the CNN input dim valid"""
         error_msg = "input_dim must be a tuple of 3 integers indicating (channels, height, width)"
         assert isinstance(self.input_dim, tuple), error_msg
-        for element in self.input_dim: assert isinstance(element, int) and element > 0
+        for element in self.input_dim:
+            assert isinstance(element, int) and element > 0
 
     def check_CNN_layers_valid(self):
         """Checks that the user inputs for cnn_hidden_layers were valid. cnn_hidden_layers must be a list of layers where
         each layer must be of one of these forms:
         - ["conv", channels, kernel_size, stride, padding]
         - ["maxpool", kernel_size, stride, padding]
+        - ["maxunpool", kernel_size, stride, padding]
         - ["avgpool", kernel_size, stride, padding]
         - ["adaptivemaxpool", output height, output width]
         - ["adaptiveavgpool", output height, output width]
         - ["linear", out]
         """
-        error_msg_layer_type = "First element in a layer specification must be one of {}".format(self.valid_cnn_hidden_layer_types)
+        error_msg_layer_type = "First element in a layer specification must be one of {}".format(
+            self.valid_cnn_hidden_layer_types)
         error_msg_conv_layer = """Conv layer must be of form ['conv', channels, kernel_size, stride, padding] where the 
                                final 4 elements are non-negative integers"""
         error_msg_maxpool_layer = """Maxpool layer must be of form ['maxpool', kernel_size, stride, padding] where the 
                                        final 2 elements are non-negative integers"""
+        error_msg_maxunpool_layer = """Maxunpool layer must be of form ['maxunpool', kernel_size, stride, padding] where the
+                                        final 2 elements are non-negative integers"""
         error_msg_avgpool_layer = """Avgpool layer must be of form ['avgpool', kernel_size, stride, padding] where the 
                                                final 2 elements are non-negative integers"""
         error_msg_adaptivemaxpool_layer = """Adaptivemaxpool layer must be of form ['adaptivemaxpool', output height, output width]"""
@@ -102,37 +111,56 @@ class CNN(nn.Module, Base_Network):
             assert isinstance(layer, list), "Each layer must be a list"
             assert isinstance(layer[0], str), error_msg_layer_type
             layer_type_name = layer[0].lower()
-            assert layer_type_name in self.valid_cnn_hidden_layer_types, "Layer name {} not valid, use one of {}".format(layer_type_name, self.valid_cnn_hidden_layer_types)
+            assert layer_type_name in self.valid_cnn_hidden_layer_types, "Layer name {} not valid, use one of {}".format(
+                layer_type_name, self.valid_cnn_hidden_layer_types)
             if layer_type_name == "conv":
                 assert len(layer) == 5, error_msg_conv_layer
-                for ix in range(3): assert isinstance(layer[ix+1], int) and layer[ix+1] > 0, error_msg_conv_layer
+                for ix in range(3):
+                    assert isinstance(layer[ix+1], int) and layer[ix+1] > 0, error_msg_conv_layer
                 assert isinstance(layer[4], int) and layer[4] >= 0, error_msg_conv_layer
             elif layer_type_name == "maxpool":
                 assert len(layer) == 4, error_msg_maxpool_layer
-                for ix in range(2): assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_maxpool_layer
-                if layer[1] != layer[2]: print("NOTE that your maxpool kernel size {} isn't the same as your stride {}".format(layer[1], layer[2]))
-                assert isinstance(layer[3], int) and layer[3] >= 0, error_msg_conv_layer
+                for ix in range(2):
+                    assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_maxpool_layer
+                if layer[1] != layer[2]:
+                    print("NOTE that your maxpool kernel size {} isn't the same as your stride {}".format(
+                        layer[1], layer[2]))
+                assert isinstance(layer[3], int) and layer[3] >= 0, error_msg_maxpool_layer
+            elif layer_type_name == "maxunpool":
+                assert len(layer) == 4, error_msg_maxunpool_layer
+                for ix in range(2):
+                    assert(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_maxunpool_layer
+                if layer[1] != layer[2]:
+                    print("NOTE that your maxunpool kernel size {} isn't the same as your stride {}".format(
+                        layer[1], layer[2]))
+                assert isinstance(layer[3], int) and layer[3] >= 0, error_msg_maxunpool_layer
             elif layer_type_name == "avgpool":
                 assert len(layer) == 4, error_msg_avgpool_layer
-                for ix in range(2): assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_avgpool_layer
+                for ix in range(2):
+                    assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_avgpool_layer
                 assert isinstance(layer[3], int) and layer[3] >= 0, error_msg_conv_layer
-                if layer[1] != layer[2]:print("NOTE that your avgpool kernel size {} isn't the same as your stride {}".format(layer[1], layer[2]))
+                if layer[1] != layer[2]:
+                    print("NOTE that your avgpool kernel size {} isn't the same as your stride {}".format(
+                        layer[1], layer[2]))
             elif layer_type_name == "adaptivemaxpool":
                 assert len(layer) == 3, error_msg_adaptivemaxpool_layer
-                for ix in range(2): assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_adaptivemaxpool_layer
+                for ix in range(2):
+                    assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_adaptivemaxpool_layer
             elif layer_type_name == "adaptiveavgpool":
                 assert len(layer) == 3, error_msg_adaptiveavgpool_layer
-                for ix in range(2): assert isinstance(layer[ix + 1], int) and layer[
-                    ix + 1] > 0, error_msg_adaptiveavgpool_layer
+                for ix in range(2):
+                    assert isinstance(layer[ix + 1], int) and layer[ix + 1] > 0, error_msg_adaptiveavgpool_layer
             elif layer_type_name == "linear":
                 assert len(layer) == 2, error_msg_linear_layer
-                for ix in range(1): assert isinstance(layer[ix+1], int) and layer[ix+1] > 0
+                for ix in range(1):
+                    assert isinstance(layer[ix+1], int) and layer[ix+1] > 0
             else:
                 raise ValueError("Invalid layer name")
 
         rest_must_be_linear = False
         for ix, layer in enumerate(all_layers):
-            if rest_must_be_linear: assert layer[0].lower() == "linear", "If have linear layers then they must come at end"
+            if rest_must_be_linear:
+                assert layer[0].lower() == "linear", "If have linear layers then they must come at end"
             if layer[0].lower() == "linear":
                 rest_must_be_linear = True
 
@@ -141,7 +169,8 @@ class CNN(nn.Module, Base_Network):
         cnn_hidden_layers = nn.ModuleList([])
         input_dim = self.input_dim
         for layer in self.layers_info[:-1]:
-            input_dim = self.create_and_append_layer(input_dim, layer, cnn_hidden_layers)
+            input_dim = self.create_and_append_layer(
+                input_dim, layer, cnn_hidden_layers)
         self.input_dim_into_final_layer = input_dim
         return cnn_hidden_layers
 
@@ -152,20 +181,27 @@ class CNN(nn.Module, Base_Network):
             layer_name, self.valid_cnn_hidden_layer_types)
         if layer_name == "conv":
             list_to_append_layer_to.extend([nn.Conv2d(in_channels=input_dim[0], out_channels=layer[1], kernel_size=layer[2],
-                                                stride=layer[3], padding=layer[4])])
+                                                      stride=layer[3], padding=layer[4])])
         elif layer_name == "maxpool":
             list_to_append_layer_to.extend([nn.MaxPool2d(kernel_size=layer[1],
-                                                   stride=layer[2], padding=layer[3])])
+                                                         stride=layer[2], padding=layer[3])])
+        elif layer_name == "maxunpool":
+            list_to_append_layer_to.extend(
+                [nn.MaxUnpool2d(kernel_size=layer[1], stride=layer[2], padding=layer[3])])
         elif layer_name == "avgpool":
             list_to_append_layer_to.extend([nn.AvgPool2d(kernel_size=layer[1],
-                                                   stride=layer[2], padding=layer[3])])
+                                                         stride=layer[2], padding=layer[3])])
         elif layer_name == "adaptivemaxpool":
-            list_to_append_layer_to.extend([nn.AdaptiveMaxPool2d(output_size=(layer[1], layer[2]))])
+            list_to_append_layer_to.extend(
+                [nn.AdaptiveMaxPool2d(output_size=(layer[1], layer[2]))])
         elif layer_name == "adaptiveavgpool":
-            list_to_append_layer_to.extend([nn.AdaptiveAvgPool2d(output_size=(layer[1], layer[2]))])
+            list_to_append_layer_to.extend(
+                [nn.AdaptiveAvgPool2d(output_size=(layer[1], layer[2]))])
         elif layer_name == "linear":
-            if isinstance(input_dim, tuple): input_dim = np.prod(np.array(input_dim))
-            list_to_append_layer_to.extend([nn.Linear(in_features=input_dim, out_features=layer[1])])
+            if isinstance(input_dim, tuple):
+                input_dim = np.prod(np.array(input_dim))
+            list_to_append_layer_to.extend(
+                [nn.Linear(in_features=input_dim, out_features=layer[1])])
         else:
             raise ValueError("Wrong layer name")
         input_dim = self.calculate_new_dimensions(input_dim, layer)
@@ -186,6 +222,12 @@ class CNN(nn.Module, Base_Network):
             new_height = int((input_dim[1] - kernel + 2*padding)/stride) + 1
             new_width = int((input_dim[2] - kernel + 2 * padding) / stride) + 1
             output_dim = (new_channels, new_height, new_width)
+        elif layer_name == "maxunpool":
+            new_channels = input_dim[0]
+            kernel, stride, padding = layer[1], layer[2], layer[3]
+            new_height = int((input_dim[1] - 1) * stride - 2 * padding + kernel)
+            new_width = int((input_dim[2] - 1) * stride - 2 * padding + kernel)
+            output_dim = (new_channels, new_height, new_width)
         elif layer_name in ["adaptivemaxpool", "adaptiveavgpool"]:
             new_channels = input_dim[0]
             output_dim = (new_channels, layer[1], layer[2])
@@ -197,17 +239,20 @@ class CNN(nn.Module, Base_Network):
         """Creates the output layers in the network"""
         output_layers = nn.ModuleList([])
         input_dim = self.input_dim_into_final_layer
-        if not isinstance(self.layers_info[-1][0], list)  : self.layers_info[-1] = [self.layers_info[-1]]
+        if not isinstance(self.layers_info[-1][0], list):
+            self.layers_info[-1] = [self.layers_info[-1]]
         for output_layer in self.layers_info[-1]:
-            self.create_and_append_layer(input_dim, output_layer, output_layers)
+            self.create_and_append_layer(
+                input_dim, output_layer, output_layers)
         return output_layers
 
     def initialise_all_parameters(self):
         """Initialises the parameters in the linear and embedding layers"""
-        initialisable_layers = [layer for layer in self.hidden_layers if not type(layer) in self.valid_layer_types_with_no_parameters]
+        initialisable_layers = [layer for layer in self.hidden_layers if not type(
+            layer) in self.valid_layer_types_with_no_parameters]
         self.initialise_parameters(nn.ModuleList(initialisable_layers))
         output_initialisable_layers = [layer for layer in self.output_layers if
-                                not type(layer) in self.valid_layer_types_with_no_parameters]
+                                       not type(layer) in self.valid_layer_types_with_no_parameters]
         self.initialise_parameters(output_initialisable_layers)
 
     def create_batch_norm_layers(self):
@@ -216,29 +261,35 @@ class CNN(nn.Module, Base_Network):
         for layer in self.layers_info[:-1]:
             layer_type = layer[0].lower()
             if layer_type == "conv":
-                batch_norm_layers.extend([nn.BatchNorm2d(num_features=layer[1])])
+                batch_norm_layers.extend(
+                    [nn.BatchNorm2d(num_features=layer[1])])
             elif layer_type == "linear":
-                batch_norm_layers.extend([nn.BatchNorm1d(num_features=layer[1])])
+                batch_norm_layers.extend(
+                    [nn.BatchNorm1d(num_features=layer[1])])
         return batch_norm_layers
 
     def forward(self, x):
         """Forward pass for the network. Note that it expects input data in the form (Batch, Channels, Height, Width)"""
-        if not self.checked_forward_input_data_once: self.check_input_data_into_forward_once(x)
+        if not self.checked_forward_input_data_once:
+            self.check_input_data_into_forward_once(x)
         x = self.process_hidden_layers(x)
         out = self.process_output_layers(x)
-        if self.y_range: out = self.y_range[0] + (self.y_range[1] - self.y_range[0])*nn.Sigmoid()(out)
+        if self.y_range:
+            out = self.y_range[0] + (self.y_range[1] -
+                                     self.y_range[0])*nn.Sigmoid()(out)
         return out
 
     def check_input_data_into_forward_once(self, x):
         """Checks the input data into forward is of the right format. Then sets a flag indicating that this has happened once
         so that we don't keep checking as this would slow down the model too much"""
         assert len(x.shape) == 4, "x should have the shape (batch_size, channel, height, width)"
-        assert x.shape[1:] == self.input_dim, "Input data must be of shape (channels, height, width) that you provided, not of shape {}".format(x.shape[1:])
-        self.checked_forward_input_data_once = True #So that it doesn't check again
+        assert x.shape[1:] == self.input_dim, "Input data must be of shape (channels, height, width) that you provided, not of shape {}".format(
+            x.shape[1:])
+        self.checked_forward_input_data_once = True  # So that it doesn't check again
 
     def process_hidden_layers(self, x):
         """Puts the data x through all the hidden layers"""
-        flattened=False
+        flattened = False
         valid_batch_norm_layer_ix = 0
         for layer_ix, layer in enumerate(self.hidden_layers):
             if type(layer) in self.valid_layer_types_with_no_parameters:
@@ -247,21 +298,28 @@ class CNN(nn.Module, Base_Network):
                 if type(layer) == nn.Linear and not flattened:
                     x = self.flatten_tensor(x)
                     flattened = True
-                x = self.get_activation(self.hidden_activations, layer_ix)(layer(x))
+                x = self.get_activation(
+                    self.hidden_activations, layer_ix)(layer(x))
                 if self.batch_norm:
                     x = self.batch_norm_layers[valid_batch_norm_layer_ix](x)
                     valid_batch_norm_layer_ix += 1
-                if self.dropout != 0.0: x = self.dropout_layer(x)
-        if not flattened: x = self.flatten_tensor(x)
+                if self.dropout != 0.0:
+                    x = self.dropout_layer(x)
+        if not flattened:
+            x = self.flatten_tensor(x)
         return x
 
     def process_output_layers(self, x):
         """Puts the data x through all the output layers"""
         out = None
         for output_layer_ix, output_layer in enumerate(self.output_layers):
-            activation = self.get_activation(self.output_activation, output_layer_ix)
+            activation = self.get_activation(
+                self.output_activation, output_layer_ix)
             temp_output = output_layer(x)
-            if activation is not None: temp_output = activation(temp_output)
-            if out is None: out = temp_output
-            else: out = torch.cat((out, temp_output), dim=1)
+            if activation is not None:
+                temp_output = activation(temp_output)
+            if out is None:
+                out = temp_output
+            else:
+                out = torch.cat((out, temp_output), dim=1)
         return out
